16b19ee7f8db48cd47b617714a0bdf266a87e43d
hydayat <39207145+hydayat@users.noreply.github.com>
2018-06-03 14:17:09 +0800

    Add files via upload

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,19 +1,28 @@
+//#pragma once
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include"soldier.h"
 
-class HelloWorld : public cocos2d::Scene
+class HelloWorld : public cocos2d::Layer
 {
+	cocos2d::Sprite* _player;
+
 public:
     static cocos2d::Scene* createScene();
+	virtual bool init();
 
-    virtual bool init();
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
+	cocos2d::Vec2 mouseDownPosition;//第一次点鼠标时鼠标的位置
+	static cocos2d::Vector<soldier*> vec_soldier;//存储我方小兵的容器
+	static cocos2d::Vector<soldier*> vec_chosed_soldier;//存储我方选中小兵的容器
+	static cocos2d::Vector<soldier*> vec_enemy;//存储敌方小兵的容器
+
+	void menuItem1Callback(cocos2d::Ref* pSender);
+	void menuItem2Callback(cocos2d::Ref* pSender);
+
+	void update(float dt) override;//监测所有对象生命值
+
     CREATE_FUNC(HelloWorld);
 };
 
