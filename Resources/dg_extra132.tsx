ecd60e36e78a19ec472f4aa6aa2480fb2878e06f
lgtinsea <39218002+lgtinsea@users.noreply.github.com>
2018-06-06 14:04:07 +0800

    Add files via upload

---
+++ Resources/dg_extra132.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dg_extra132" tilewidth="32" tileheight="32" tilecount="144" columns="9">
+ <image source="dg_extra132.jpg" width="288" height="512"/>
+</tileset>
---
+++ Resources/dg_iso32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dg_iso32" tilewidth="32" tileheight="32" tilecount="506" columns="23">
+ <image source="dg_iso32.jpg" width="756" height="735"/>
+</tileset>
